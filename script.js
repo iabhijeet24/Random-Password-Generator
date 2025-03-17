2f8ced353d9f69f066da1e800965ca120ba8d4e4
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-03-17 20:25:20 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+alert("hello");
+
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "%$#@&*.";
 
-const variables = upperCase+lowerCase+symbol+number;
+const variables = upperCase + lowerCase + symbol + number;
 
-let passwordLength = document.getElementById("number");
-let capacity = 0;
+let button = document.getElementById("button")
+let passwordLengthInput = document.getElementById("number");
+let passwordbox = document.getElementById("newpassword"); 
 
-function generator(){
-    
-        let password = ""; 
-    
-        for (let i = 0; i < passwordLength; i++) {
-            password += variables[Math.floor(Math.random() * variables.length)];
-        }
-    
-        return password;
-    
+function generator() {
+    let password = ""; 
+    let passwordLength = parseInt(passwordLengthInput.value); 
+
+    for (let i = 0; i < passwordLength; i++) {
+        password += variables[Math.floor(Math.random() * variables.length)];
+    }
+    passwordbox.value = password
+    // newpassword.innerText = password; // 
 }
 
-newpassword.innerText = password
